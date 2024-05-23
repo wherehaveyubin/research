fc4dc2c334b64b3a82d8065acde7a99d7b5808e8
Yubin Lee <42401542+lucyubin@users.noreply.github.com>
2024-05-23 13:44:14 +0900

    Update supply preprocessing.py

--- 2024-JKST/supply preprocessing.py
+++ 2024-JKST/supply preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import re
 import numpy as np
+import geokakao as gk
 
 file_path = 'D:/*'
 
@@ -18,13 +18,24 @@ fac_merge.to_csv(file_path + 'supply/fac_merge.csv', index=False, encoding='euc-
 # 필요한 열만 추출
 fac_merge = fac_merge[(fac_merge['종별코드명'] == '상급종합') | (fac_merge['종별코드명'] == '종합병원') | 
                       (fac_merge['종별코드명'] == '병원') | (fac_merge['종별코드명'] == '의원') | 
-                      (fac_merge['종별코드명'] == '보건의료원')] # 9058행
+                      (fac_merge['종별코드명'] == '보건의료원')] #9058행
 fac_hour = fac_merge.filter(regex='^진료')
 fac_merge = pd.concat([fac_merge[['요양기관명_x', '종별코드명', '주소', '총의사수', '응급실 야간운영여부']], fac_hour], axis=1)
 fac_merge = fac_merge[(fac_merge['주소'].str.startswith(('서울', '경상북도')))] #서울|경북 추출, 2574행
 fac_merge.rename(columns={'요양기관명_x': '기관명'}, inplace=True)
 fac_merge.replace(0, np.nan, inplace=True) #휴무일 제외 (0인 값을 nan로 대체)
 fac_merge.fillna('', inplace=True)
+
+# 지오 코딩을 위한 주소 가공
+address_correction = {
+    '서울특별시 중구 마른내로 9-9 (저동2가)': '서울특별시 중구 마른내로 9',
+    '경상북도 경주시 원화로 315 315': '경상북도 경주시 원화로 315',
+    '서울특별시 중구 다산로36길 11 3,4,5층 (신당동)': '서울특별시 중구 다산로36길 11',
+    '서울특별시 영등포구 국회대로54길 10 305,306호 (영등포동7가, 아크로타워스퀘어)': '서울특별시 영등포구 국회대로54길 10',
+    '서울특별시 강북구 도봉로 217 5,6,7층 (미아동, 25메디컬빌딩)': '서울특별시 강북구 도봉로 217',
+    '경상북도 청송군 청송읍 의료원길 19 19': '경상북도 청송군 청송읍 의료원길 19'
+}
+fac_merge['주소'] = fac_merge['주소'].replace(address_correction)
 
 # 진료 시간을 정수로 변경 
 for col in fac_merge.columns:
@@ -43,13 +54,6 @@ fac_weekday = fac_merge[(fac_merge['진료시작시간_월'] <= 600) & (fac_merg
                         (fac_merge['진료시작시간_금'] <= 600) & (fac_merge['진료시작시간_금'] != '') | 
                         (fac_merge['진료종료시간_금'] >= 2300) & (fac_merge['진료종료시간_금'] != '') |
                         (fac_merge['응급실 야간운영여부'] == 'Y')] #109행
-fac_weekday.to_csv(file_path + 'supply/fac_weekday.csv', index=False, encoding='euc-kr')
-
-fac_sl_weekday = fac_weekday[(fac_weekday['주소'].str.startswith('서울'))] #79행
-fac_sl_weekday.to_csv(file_path + 'supply/fac_sl_weekday.csv', index=False, encoding='euc-kr')
-
-fac_gb_weekday = fac_weekday[(fac_weekday['주소'].str.startswith('경상북도'))] #30행
-fac_gb_weekday.to_csv(file_path + 'supply/fac_gb_weekday.csv', index=False, encoding='euc-kr')
 
 # 주말 야간 진료 가능 병원
 fac_weekend = fac_merge[(fac_merge['진료시작시간_토'] <= 600) & (fac_merge['진료시작시간_토'] != '') | 
@@ -57,10 +61,20 @@ fac_weekend = fac_merge[(fac_merge['진료시작시간_토'] <= 600) & (fac_merg
                         (fac_merge['진료시작시간_일'] <= 600) & (fac_merge['진료시작시간_일'] != '') | 
                         (fac_merge['진료종료시간_일'] >= 2300) & (fac_merge['진료종료시간_일'] != '') |
                         (fac_merge['응급실 야간운영여부'] == 'Y')] # 107행
+
+# 지오코딩
+gk.add_coordinates_to_dataframe(fac_weekday, '주소')
+fac_weekday.to_csv(file_path + 'supply/fac_weekday.csv', index=False, encoding='euc-kr')
+
+gk.add_coordinates_to_dataframe(fac_weekend, '주소')
 fac_weekend.to_csv(file_path + 'supply/fac_weekend.csv', index=False, encoding='euc-kr')
 
-fac_sl_weekend = fac_weekend[(fac_weekend['주소'].str.startswith('서울'))] # 77행
+fac_sl_weekday = fac_weekday[(fac_weekday['주소'].str.startswith('서울'))] #79행
+fac_sl_weekend = fac_weekend[(fac_weekend['주소'].str.startswith('서울'))] #77행
+fac_sl_weekday.to_csv(file_path + 'supply/fac_sl_weekday.csv', index=False, encoding='euc-kr')
 fac_sl_weekend.to_csv(file_path + 'supply/fac_sl_weekend.csv', index=False, encoding='euc-kr')
 
-fac_gb_weekend = fac_weekend[(fac_weekend['주소'].str.startswith('경상북도'))] # 30행
+fac_gb_weekday = fac_weekday[(fac_weekday['주소'].str.startswith('경상북도'))] #30행
+fac_gb_weekend = fac_weekend[(fac_weekend['주소'].str.startswith('경상북도'))] #30행
+fac_gb_weekday.to_csv(file_path + 'supply/fac_gb_weekday.csv', index=False, encoding='euc-kr')
 fac_gb_weekend.to_csv(file_path + 'supply/fac_gb_weekend.csv', index=False, encoding='euc-kr')
