164367b4d160b7229346d7bba215e6cf9da1bb34
Yubin Lee <42401542+lucyubin@users.noreply.github.com>
2025-05-17 23:38:04 +0900

    Update heat.py

--- 2025-Heat/heat.py
+++ 2025-Heat/heat.py
@@ -4,22 +4,41 @@ pip install deweydatapy@git+https://github.com/Dewey-Data/deweydatapy
 
 import deweydatapy as ddp
 
-# Import the library and save permissions
+workspace = "D:/*"
+
+## Download data
 # API Key
-apikey = ""
+apikey = "*"
 
-# Product path
-# Weekly Patterns - Foot Traffic - Full Historical Data (data endpoint)
-pp_ = ""
+# Product path (Weekly Patterns - Foot Traffic - Full Historical Data (data endpoint))
+pp_ = "*"
 
-# Review the meta information of the data product
+# Review the metadata of the data product
 meta = ddp.get_meta(apikey, pp_, print_meta = True) # returns a dataframe with meta information
 
 # Collect the list of files to download and store in a dataframe
 files_df = ddp.get_file_list(apikey, pp_, 
-                             start_date = '2024-07-01',
-                             end_date = '2024-07-31',
-                             print_info = True);
+                             start_date = '2024-05-27',
+                             end_date = '2024-10-05',
+                             print_info = True)
 
 # Download files from the dataframe
-ddp.download_files(files_df, "D:/heat", skip_exists = True)
+ddp.download_files(files_df, workspace+"dewey", skip_exists = True)
+
+## Import downloaded data
+# Import list of files ending with csv.gz
+files = glob.glob(workspace+"dewey/" + "*csv.gz")
+
+df = DataFrame()
+
+for f in files:
+    df = pd.read_csv(f,compression='gzip', encoding='utf-8', dtype={'POI_CBG': str} )
+    break
+
+# Select Phoenix, Arizona data 
+df_az = df[
+    (df['region'].str.upper() == 'AZ') & 
+    (df['city'].str.lower() == 'phoenix')
+]
+df_az.head()
+
